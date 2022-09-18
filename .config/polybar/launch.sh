8c92fc88d532cfb8109df0b4460498fc533b8fa8
neva1ack <zanebanks2@gmail.com>
2022-09-19 04:07:39 +0930

    add: bspwm, kitty, nvim, picom, polybar, rofi, xborders

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+# Launch top bar
+polybar top
